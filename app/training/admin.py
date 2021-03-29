7781c5f73683ddfc291b9bec2b2ad7269134eefa
DmitryBovsunovskyi <dmitrybovsunovskyi@gmail.com>
2021-03-29 16:06:08 +0200

    Started training app

---
+++ app/training/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from training import models
+
+
+admin.site.register(models.Workout)
+admin.site.register(models.Exercise)
+admin.site.register(models.ExerciseSet)
+admin.site.register(models.Set)
